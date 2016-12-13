c23e9fe510b40a1f63d2346086c24cef22ebcc0f
Alicia Miceli <akb.miceli@gmail.com>
2016-12-12 21:17:12 -0600

    people and groups are linked, groups and events are linked

--- javascripts/controllers/PeopleEditCtrl.js
+++ javascripts/controllers/PeopleEditCtrl.js
@@ -7,13 +7,14 @@ app.controller("PeopleEditCtrl", function($scope, $location, $routeParams, Peopl
   PeopleFactory.getSinglePerson(personId).then(function(onePerson){
       onePerson.id = personId;
       $scope.newPerson = onePerson;
-      console.log("onePerson", onePerson);
+      console.log("onePerson", onePerson);  
   });
 
     $scope.addNewPerson = function(){
       PeopleFactory.editPerson($scope.newPerson).then(function(response){
         $scope.newPerson = {};
         $location.url("/people/list");
+        console.log("onePerson", personId);
       });
     };
 });
--- javascripts/controllers/PeopleViewCtrl.js
+++ javascripts/controllers/PeopleViewCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller("PeopleViewCtrl", function($scope, $routeParams, PeopleFactory){
+app.controller("PeopleViewCtrl", function($scope, $location, $rootScope, $routeParams, PeopleFactory, GroupFactory){
   $scope.selectedPerson = {};
   var personId = $routeParams.id;
   console.log("$routeParams", personId);
@@ -11,4 +11,25 @@ app.controller("PeopleViewCtrl", function($scope, $routeParams, PeopleFactory){
     $scope.selectedPerson = onePerson;
     console.log("onePerson", onePerson);
   });
+
+
+
+
+  $scope.addPersonToGroup = function(groupId){
+
+  	console.log("what is the current value of this group id?");
+    console.log(groupId);
+    console.log(personId);
+    GroupFactory.getSingleGroup(groupId).then(function(group){
+      group.members.push(personId);
+      GroupFactory.editGroup(group);
+    });
+  };
+
+  GroupFactory.getGroupList($rootScope.user.uid).then(function(results){
+  	$scope.groups=results;
+  });
+
+
+   $scope.groups=[];
 });
